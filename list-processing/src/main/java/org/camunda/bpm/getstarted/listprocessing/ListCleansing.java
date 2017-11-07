dd34f34dff880a977cd25b97df1c8fed79d5a508
jpb13007 <33462799+jpb13007@users.noreply.github.com>
2017-11-07 14:02:55 -0500

    Delete ListCleansing.java

--- list-processing/src/main/java/org/camunda/bpm/getstarted/listprocessing/ListCleansing.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.getstarted.listprocessing;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("List Processing")
-public class ListCleansing extends ServletProcessApplication {
-  // empty implementation
-}
