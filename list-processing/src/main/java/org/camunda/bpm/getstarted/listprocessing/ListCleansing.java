9ca3372e7371fd7c6e45c93d70845d95f729b16b
jairalei <jaira.cruz@uconn.edu>
2017-11-07 13:08:44 -0500

    Add files via upload

---
+++ list-processing/src/main/java/org/camunda/bpm/getstarted/listprocessing/ListCleansing.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.getstarted.listprocessing;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("List Processing")
+public class ListCleansing extends ServletProcessApplication {
+  // empty implementation
+}
