6414d563460daade5f6e9f5054f009e6d4c4fc1b
Amitoj Kapoor <amitoj@dotalign.com>
2017-11-07 14:37:10 -0500

    Add project

---
+++ list-processing/src/main/java/org/camunda/bpm/listprocessing/ListProcessingApplication.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.listprocessing;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("List Processing")
+public class ListProcessingApplication extends ServletProcessApplication {
+// empty implementation
+}
